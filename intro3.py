6603d1e33ff0726d56cdc5fba2d002b51089a60e
PythonMaster754 <axiao.yd@gmail.com>
2025-11-30 16:30:45 -0700

    push

---
+++ intro3.py
@@ -0,0 +1,10 @@
+# Program 3: "Type" it out
+
+print(type(45))
+print(type(3.142))
+print(type('hello'))
+print(type(True))
+print(type(None))
+print(type([1, 2, 3]))
+print(type((4, 1, 3, 2)))
+print(type(len))
---
+++ intro4.py
@@ -0,0 +1,10 @@
+# Program 4: "Slice" of code
+
+course = 'CMPUT'
+number = [101, 174, 175, 191, 201]
+new_word = course.replace(course[3], "")
+
+print(course[:1], number[1])
+print(new_word)
+print(number[3])
+print(number[2:])
