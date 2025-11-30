5a25b1ceedefc210345b7e677263aa6d8512ebb1
PythonMaster754 <axiao.yd@gmail.com>
2025-11-30 16:28:24 -0700

    push

---
+++ intro2.py
@@ -0,0 +1,5 @@
+# Program 2: Getting to know you more
+
+movie = (str(input("Enter your favorite movie:")))
+celebrity = (str(input("Enter your favorite celebrity:")))
+print(f"My favorite movie is{movie} and I love{celebrity}.")
